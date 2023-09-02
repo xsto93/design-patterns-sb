63f61e7feb0504109057f7bd12f30e28e7f4a57c
xsto93 <xsto93@gmail.com>
2023-09-02 17:31:28 +0200

    Add files via upload
    adding storybook

---
+++ assets/Creacionales-51366378.js
@@ -0,0 +1,11 @@
+import{j as n}from"./jsx-runtime-ffb262ed.js";import{u as o}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";function s(i){const e=Object.assign({h2:"h2",p:"p",ul:"ul",li:"li"},o(),i.components);return n.jsxs(n.Fragment,{children:[n.jsx(e.h2,{id:"patrones-creacionales",children:"Patrones Creacionales"}),`
+`,n.jsx(e.p,{children:"Los patrones creacionales proporcionan varios mecanismos de creación de objetos que incrementan la flexibilidad y la reutilización del código existente."}),`
+`,n.jsx(e.p,{children:"Los patrones creacionales son:"}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsx(e.li,{children:"Factory Method"}),`
+`,n.jsx(e.li,{children:"Abstract Factory"}),`
+`,n.jsx(e.li,{children:"Builder"}),`
+`,n.jsx(e.li,{children:"Prototype"}),`
+`,n.jsx(e.li,{children:"Singleton"}),`
+`]})]})}function l(i={}){const{wrapper:e}=Object.assign({},o(),i.components);return e?n.jsx(e,Object.assign({},i,{children:n.jsx(s,i)})):s(i)}export{l as default};
+//# sourceMappingURL=Creacionales-51366378.js.map
---
+++ assets/Introduccion-bf8bd822.js
@@ -0,0 +1,10 @@
+import{j as e}from"./jsx-runtime-ffb262ed.js";import{u as t}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";function o(s){const n=Object.assign({h1:"h1",p:"p",strong:"strong",ul:"ul",li:"li"},t(),s.components);return e.jsxs(e.Fragment,{children:[e.jsx(n.h1,{id:"introducción-a-los-patrones-de-diseño",children:"Introducción a los patrones de diseño"}),`
+`,e.jsxs(n.p,{children:["Los patrones de diseño (",e.jsx(n.strong,{children:"design patterns"}),`) son soluciones habituales a problemas comunes en el diseño de software. Cada patrón es como un plano que se\r
+puede personalizar para resolver un problema de diseño particular de tu código.`]}),`
+`,e.jsx(n.p,{children:"Se dividen en 3 tipos de patrón según su comportamiento."}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Creacionales"}),`
+`,e.jsx(n.li,{children:"Estructurales"}),`
+`,e.jsx(n.li,{children:"De comportamiento"}),`
+`]})]})}function d(s={}){const{wrapper:n}=Object.assign({},t(),s.components);return n?e.jsx(n,Object.assign({},s,{children:e.jsx(o,s)})):o(s)}export{d as default};
+//# sourceMappingURL=Introduccion-bf8bd822.js.map
